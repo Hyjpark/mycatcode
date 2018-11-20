4807a477aab3f3263061045ee91f71e64917d233
Hyejung Park <hyej042@gmail.com>
2018-11-20 21:15:30 +0900

    #10 Add lof in to activity

--- app/src/main/java/kr/emirim/hyejungpark/mycatcode/SplashActivity.java
+++ app/src/main/java/kr/emirim/hyejungpark/mycatcode/SplashActivity.java
@@ -2,12 +2,14 @@ package kr.emirim.hyejungpark.mycatcode;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SplashActivity extends AppCompatActivity {
-
+    public static  final String TAG = "스플래시액티비티";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        Log.d(TAG, "onCreate 실행");
     }
 }
